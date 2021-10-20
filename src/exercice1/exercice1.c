310c85922ba386a052fc0372d8e6985131736b6e
AlexisVillaroya <alexisvillaroya2a@gmail.com>
2021-10-20 09:46:35 +0200

    [feat] Structure Projet / Exercice 1

---
+++ src/exercice1/exercice1.c
@@ -0,0 +1,31 @@
+/**
+ * Recherche par dichotomie dans un tableau d'entiers
+ * @param array The array of values
+ * @param size_t The size of the array
+ * @param value The value to find
+ * @return The position of the value found or -1
+ */
+
+
+
+int find_by_dichotomy(int array[], int size_t, int value){
+    int position, first, last, mid;
+    first = array[0];
+    last = array[size_t-1];
+    mid = (first + last) / 2;
+
+    while(first <= 1){
+        if (array[mid] < value){
+            first = mid + 1;
+            position = mid + 1;
+        }else if(array[mid] == value){
+            position = mid;
+            break;
+        }else{
+            last = mid - 1;
+            position = mid - 1;
+        }
+        mid = (first + last) / 2;
+    }
+    return position;
+}
---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <assert.h>
+#include "exercice1/exercice1.h"
+
+int main()
+{
+	int array[] = {1,2,3,4,5,6,7,8,9,10};
+	int test = find_by_dichotomy(array, 10, 4);
+	printf("%d\n", test); // 3
+	assert(find_by_dichotomy(array, 10, 4) == 3);
+	return (0);
+}
